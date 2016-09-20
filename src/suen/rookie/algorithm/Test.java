981f0aa86a5a7f417e3364cb76da945264ee3410
bjhksun <shk8000@163.com>
2016-09-20 12:38:13 +0800

    Test

---
+++ src/suen/rookie/algorithm/Test.java
@@ -0,0 +1,12 @@
+package suen.rookie.algorithm;
+
+/**
+ * Created by bjhksun on 2016/9/20.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("testing");
+    }
+
+}
